f21977ab28ad09564b5e45d7ae55c9428fb1f581
Rezvan Nafee <rezvan.nafee@stonybrook.edu>
2022-10-17 01:54:22 -0400

    Changed status code to handle error handling better when deleting lists and getting list from the backend.

--- server/controllers/playlist-controller.js
+++ server/controllers/playlist-controller.js
@@ -89,7 +89,7 @@ deletePlaylistyById = async (req,res) => {
 getPlaylistById = async (req, res) => {
     await Playlist.findOne({ _id: req.params.id }, (err, list) => {
         if (err) {
-            return res.status(400).json({ success: false, error: err })
+            return res.status(204).json({ success: false, error: err })
         }
 
         return res.status(200).json({ success: true, playlist: list })
